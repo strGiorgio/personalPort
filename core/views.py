bacb0b2f18492abdb91b79322b1ee327163a6163
Giorgio Alefe <giorgioa2002@gmail.com>
2023-06-22 13:33:55 -0300

    Create a context data in IndexView

--- core/views.py
+++ core/views.py
@@ -1,6 +1,14 @@
+from typing import Any, Dict
 from django.shortcuts import render
 from django.views.generic import TemplateView
 # Create your views here.
 
 class IndexView(TemplateView):
     template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(IndexView, self).get_context_data(**kwargs)
+
+        context['title'] = 'Making great things!'
+
+        return context
