0b50e654fa691332fbce4f35a3855cca13deb028
strGiorgio <giorgioa2002@gmail.com>
2023-10-21 21:10:15 -0300

    Fix arrows, create a code to change projects and add projects image in html

--- core/static/js/option3-mainmenu.js
+++ core/static/js/option3-mainmenu.js
@@ -1,4 +1,3 @@
-const projectTwo = document.querySelector(".projectTwo")
 var num = 1
 
 function previous() {
@@ -8,15 +7,40 @@ function previous() {
     else {
         num = 2;
     }
-     console.log(num) 
+    console.log(num) 
+    changeProject()
 }
 
 function next() {
     if (num < 2){
         num ++
     }
+
     else{
         num = 0;
     }
     console.log(num)
+    changeProject()
+}
+
+function changeProject() {
+    const projectOne = document.querySelector('.projectOne');
+    const projectTwo = document.querySelector('.projectTwo');
+    const projectThree = document.querySelector('.projectThree');
+
+    if (num == 0){
+        projectOne.style.display = "flex";
+        projectTwo.style.display = "none";
+        projectThree.style.display = "none";
+    }
+    else if (num == 1){
+        projectOne.style.display = "none";
+        projectTwo.style.display = "flex";
+        projectThree.style.display = "none";
+    }
+    else {
+        projectOne.style.display = "none";
+        projectTwo.style.display = "none";
+        projectThree.style.display = "flex";
+    }
 }
